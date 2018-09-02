17923e4e839855a1db17574f188bbbf296cd0a09
Rachel Wyatt <rw126@evansville.edu>
2018-09-02 14:32:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(sentence)
   word_array = sentence.split(/ /)
   word_array.collect do |word|
     word.reverse
+    word_array.join
   end
-  word_array.join
+  word_array
 end
